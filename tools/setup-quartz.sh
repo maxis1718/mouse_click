6379b4976f8d935633fa244fab2bcc6fb95f680e
maxis <maxis1718@gmail.com>
2014-10-31 17:20:28 +0800

    setup script for Quartz (Mac)

---
+++ tools/setup-quartz.sh
@@ -0,0 +1,7 @@
+wget https://pypi.python.org/packages/source/p/pyobjc-framework-Quartz/pyobjc-framework-Quartz-3.0.3.tar.gz
+tar -xf pyobjc-framework-Quartz-3.0.3.tar.gz
+
+cd pyobjc-framework-Quartz-3.0.3
+python setup.py build
+python setup.py install
+
